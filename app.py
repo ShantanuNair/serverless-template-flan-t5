90e438406deeb78795d5cc3fc4245740aee2ec4c
Shantanu Nair <ShantanuNair@users.noreply.github.com>
2023-01-27 11:44:30 +0530

    Add max_length to user input

--- app.py
+++ app.py
@@ -21,12 +21,13 @@ def inference(model_inputs:dict) -> dict:
 
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
+    max_length = model_inputs.get('max_length', 100)
     if prompt == None:
         return {'message': "No prompt provided"}
     
     # Run the model
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
-    output = model.generate(input_ids, max_length=100)
+    output = model.generate(input_ids, max_length)
     result = tokenizer.decode(output[0], skip_special_tokens=True)
 
     # Return the results as a dictionary
